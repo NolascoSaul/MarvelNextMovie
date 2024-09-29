bae9ae99323ac47fd42069c28b2a349652c14cdf
Saúl <sauln559@gmail.com>
2024-09-29 02:56:10 -0500

    templates

---
+++ templates/main.php
@@ -0,0 +1,16 @@
+<main>
+    <section>
+        <img 
+            src="<?= $poster_url ?>" 
+            width="300" 
+            style= "border-radius: 6px;"
+            alt="Poster de <?= $title; ?> "
+        >
+    </section>
+
+    <hgroup>
+        <h3><?= $title ?> - <?= $until_message ?> </h3>
+        <p>Fecha de estreno: <?= $release_date ?></p>
+        <p>La siguiente película es <?= $following_production["title"] ?></p>
+    </hgroup>
+</main>
---
+++ templates/styles.php
@@ -0,0 +1,18 @@
+<style>
+    main{
+        display: grid;
+        place-content: center;
+    }
+        
+    section{
+        display: flex;
+        justify-content: center;
+        text-align: center;
+    }
+
+    hgroup{
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+    }
+</style>
